ff3e0cceb32d30e5d554ee67af48a5141bc50a1c
Catalina Pimentel <cpimentel@buk.cl>
2024-06-03 10:39:34 +0200

    chore: setup ruby on rails project

---
+++ config/initializers/annotate.rb
@@ -0,0 +1,26 @@
+# config/initializers/annotate.rb
+Annotate.set_defaults(
+  'position_in_class' => 'before',
+  'show_indexes' => 'true',
+  'simple_indexes' => 'false',
+  'model_dir' => 'app/models',
+  'root_dir' => '',
+  'include_version' => 'false',
+  'require' => '',
+  'exclude_tests' => 'true',
+  'exclude_fixtures' => 'true',
+  'exclude_factories' => 'true',
+  'ignore_model_sub_dir' => 'false',
+  'ignore_columns' => nil,
+  'ignore_unknown_models' => 'false',
+  'hide_limit_and_precision' => 'false',
+  'skip_on_db_migrate' => 'false',
+  'format_bare' => 'true',
+  'format_rdoc' => 'false',
+  'format_yard' => 'false',
+  'sort' => 'false',
+  'force' => 'false',
+  'trace' => 'false',
+  'wrapper_open' => nil,
+  'wrapper_close' => nil
+)
